db65413a6b0528d5c788b6db7fb8cd26f997ed09
zhangnan007 <https://zhangnan007@github.com>
2020-03-26 23:30:41 +0800

    commit

---
+++ src/main/java/com/bar/kushiage/controller/MealNumberController.java
@@ -0,0 +1,13 @@
+package com.bar.kushiage.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 取餐号controller
+ */
+@Controller
+@RequestMapping("/mealNum")
+public class MealNumberController {
+
+}
