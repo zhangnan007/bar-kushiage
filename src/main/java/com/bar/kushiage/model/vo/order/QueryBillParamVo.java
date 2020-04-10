7f174508875161ab147aa291fb0a81d5b14bd762
zhangnan007 <https://zhangnan007@github.com>
2020-04-10 23:03:30 +0800

    commit

--- src/main/java/com/bar/kushiage/model/vo/order/QueryBillParamVo.java
+++ src/main/java/com/bar/kushiage/model/vo/order/QueryBillParamVo.java
@@ -3,13 +3,13 @@ package com.bar.kushiage.model.vo.order;
 import lombok.Data;
 
 import java.sql.Timestamp;
-import java.util.Date;
 
 /**
  * 根据条件查询账单数据Vo
  */
 @Data
 public class QueryBillParamVo {
+    private Integer page;
     private Double conPrice; // 消费金额
     private Integer mealNum; // 取餐号
     private String orderNum; // 订单号
@@ -18,5 +18,5 @@ public class QueryBillParamVo {
     private Timestamp startTime; // 后台逻辑用开始时间
     private Timestamp endTime; // 后台逻辑用结束时间
     private Integer pageNum = 1; // 当前页码
-    private Integer pageSize = 2; // 每页显示条数
+    private Integer pageSize = 1; // 每页显示条数
 }
